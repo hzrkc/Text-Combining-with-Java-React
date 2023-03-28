fb1f91d026543a421a598a0967f8dbd0d112b4a2
Hazar Koc <hazark41@gmail.com>
2023-03-28 22:48:09 +0300

    Update MetinService.java

--- metin/src/main/java/dev/hazar/metin/MetinService.java
+++ metin/src/main/java/dev/hazar/metin/MetinService.java
@@ -25,12 +25,37 @@ public class MetinService {
 
     public String combineMetins(Map<Integer, String> payload){
 
+        long startTime = 0, endTime = 0;
+        startTime = System.nanoTime();
+
+
         Collection<String> values = payload.values();
         String[] targetArray = values.toArray(new String[0]);
 
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < targetArray.length; i++) {
+        int b;
+
+        result.append(targetArray[0]);
+
+        for(int a = 1; a < targetArray.length; a++){
+
+                for( b=0 ; b < targetArray[a].length(); b++){
+
+                    if(result.toString().contains(targetArray[a].substring(0,b)))
+                    {
+                        continue;
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+                result.append(targetArray[a].substring(b-1));
+        }
+
+        /*for (int i = 0; i < targetArray.length; i++) {
             String[] words = targetArray[i].split(" ");
 
             for (int j = 0; j < words.length; j++) {
@@ -38,9 +63,16 @@ public class MetinService {
                     result.append(words[j]).append(" ");
                 }
             }
-        }
+        }*/
+        endTime = System.nanoTime();
 
+        Double fark = (((double) (endTime - startTime)) / 1000000);
+
+        String sure = fark.toString();
+
+        result.append(" | Çalışma Süresi: " + sure + " ms");
         String output = result.toString().trim();
+
 
         return output;
     }
